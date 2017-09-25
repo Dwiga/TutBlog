3b4403d8cdcb145e04577e8a22e6f5afa40873e6
dwiga <dwi.hyuga@gmail.com>
2017-09-25 20:50:54 +0700

    Tutorial java bagian 9 : operasi matematis dengan jython

--- python/process.py
+++ python/process.py
@@ -1,3 +1,11 @@
 class pertamax:
     def satu(self, isi):
         return isi + " + and we love python"
+
+    def dua(self, angsat, angdu, proc):
+        if proc == "+":
+            a = int(angsat) + int(angdu)
+            return a
+        elif proc == '-':
+            b = int(angsat) - int(angdu)
+            return b
--- src/dft_teman.java
+++ src/dft_teman.java
@@ -6,6 +6,7 @@ import java.text.FieldPosition;
 import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import javax.swing.Action;
 import javax.swing.JFrame;
 import org.python.core.PyObject;
 import org.python.util.PythonInterpreter;
@@ -45,6 +46,30 @@ public class dft_teman extends javax.swing.JFrame {
                 plcTxt1.setText(feedback.toString());
             }
         });
+        
+        addbtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                py.execfile("python/process.py");
+                py.exec("angsat = '"+ angsat.getText() +"'");
+                py.exec("angdu = '"+ angdu.getText() +"'");
+                py.exec("proc = '+'");
+                PyObject feedback = py.eval("repr(pertamax().dua(angsat, angdu, proc))");
+                equals.setText(feedback.toString());
+            }
+        });
+        
+        minbtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                py.execfile("python/process.py");
+                py.exec("angsat = '"+ angsat.getText() +"'");
+                py.exec("angdu = '"+ angdu.getText() +"'");
+                py.exec("proc = '-'");
+                PyObject feedback = py.eval("repr(pertamax().dua(angsat, angdu, proc))");
+                equals.setText(feedback.toString());
+            }
+        });
     }
 
     /**
@@ -71,6 +96,15 @@ public class dft_teman extends javax.swing.JFrame {
         btnAct = new javax.swing.JButton();
         jXDatePicker1 = new org.jdesktop.swingx.JXDatePicker();
         plcTxt1 = new javax.swing.JTextField();
+        angsat = new javax.swing.JTextField();
+        jLabel5 = new javax.swing.JLabel();
+        angdu = new javax.swing.JTextField();
+        jLabel6 = new javax.swing.JLabel();
+        addbtn = new javax.swing.JToggleButton();
+        minbtn = new javax.swing.JToggleButton();
+        dividebtn = new javax.swing.JButton();
+        multiplybtn = new javax.swing.JButton();
+        equals = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -93,15 +127,26 @@ public class dft_teman extends javax.swing.JFrame {
 
         btnAct.setText("Action!");
 
+        jLabel5.setText("angka 1");
+
+        jLabel6.setText("angka 2");
+
+        addbtn.setText("+");
+
+        minbtn.setText("-");
+
+        dividebtn.setText("/");
+
+        multiplybtn.setText("*");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(btnProcess)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -118,21 +163,46 @@ public class dft_teman extends javax.swing.JFrame {
                             .addComponent(txtNickname)
                             .addComponent(txtAlamat)
                             .addComponent(txtNohp, javax.swing.GroupLayout.DEFAULT_SIZE, 121, Short.MAX_VALUE)
-                            .addComponent(txtUsia))))
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
+                            .addComponent(txtUsia))
                         .addGap(37, 37, 37)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(plcTxt1, javax.swing.GroupLayout.Alignment.TRAILING)
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(jXDatePicker1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(0, 387, Short.MAX_VALUE))
-                            .addComponent(plcTxt, javax.swing.GroupLayout.DEFAULT_SIZE, 491, Short.MAX_VALUE))
-                        .addContainerGap())
+                            .addComponent(plcTxt, javax.swing.GroupLayout.DEFAULT_SIZE, 491, Short.MAX_VALUE)))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(18, 18, 18)
-                        .addComponent(btnAct)
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(174, 174, 174)
+                                .addComponent(btnProcess)
+                                .addGap(18, 18, 18)
+                                .addComponent(btnAct))
+                            .addGroup(layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(angsat, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(21, 21, 21)
+                                .addComponent(angdu, javax.swing.GroupLayout.PREFERRED_SIZE, 104, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(34, 34, 34)
+                                .addComponent(jLabel5)
+                                .addGap(85, 85, 85)
+                                .addComponent(jLabel6))
+                            .addGroup(layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(addbtn, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(minbtn)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(dividebtn)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(multiplybtn)))
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(equals, javax.swing.GroupLayout.PREFERRED_SIZE, 211, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -166,11 +236,27 @@ public class dft_teman extends javax.swing.JFrame {
                     .addComponent(jLabel4)
                     .addComponent(txtUsia, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jXDatePicker1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(49, 49, 49)
+                .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(btnProcess)
-                    .addComponent(btnAct))
-                .addContainerGap(144, Short.MAX_VALUE))
+                    .addComponent(btnAct)
+                    .addComponent(btnProcess))
+                .addGap(19, 19, 19)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel5)
+                    .addComponent(jLabel6))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(angsat, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(angdu, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(addbtn)
+                    .addComponent(minbtn)
+                    .addComponent(dividebtn)
+                    .addComponent(multiplybtn))
+                .addGap(18, 18, 18)
+                .addComponent(equals, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(22, Short.MAX_VALUE))
         );
 
         pack();
@@ -227,13 +313,22 @@ public class dft_teman extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel Nickname;
+    private javax.swing.JToggleButton addbtn;
+    private javax.swing.JTextField angdu;
+    private javax.swing.JTextField angsat;
     private javax.swing.JButton btnAct;
     private javax.swing.JButton btnProcess;
+    private javax.swing.JButton dividebtn;
+    private javax.swing.JTextField equals;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
     private org.jdesktop.swingx.JXDatePicker jXDatePicker1;
+    private javax.swing.JToggleButton minbtn;
+    private javax.swing.JButton multiplybtn;
     private javax.swing.JTextField plcTxt;
     private javax.swing.JTextField plcTxt1;
     private javax.swing.JTextField txtAlamat;
